d39dba2ec5d9078c61e58ca3457d33b2adc88e7c
lucas-shao <lucas_shao@126.com>
2023-06-11 21:56:58 +0800

    第一版交互，能够做到听懂问题，回答问题

---
+++ app.py
@@ -0,0 +1,16 @@
+from wakeword.wakeword import PicoWakeWord
+from speech.speech2text import AzureASR
+from speech.text2speech import AzureTTS
+
+
+picowakeword = PicoWakeWord()
+print("开始监听唤醒词...")
+while True:
+    keyword_idx = picowakeword.detect_wake_word()
+    if keyword_idx >= 0:
+        print("我被唤醒了！")
+        question = AzureASR().speech_to_text()
+        print("你问：{}".format(question))
+        answer = question
+        print("我答：{}".format(answer))
+        AzureTTS().text_to_speech_and_play(answer)
--- wakeword/wakeword.py
+++ wakeword/wakeword.py
@@ -3,31 +3,42 @@ import pyaudio
 import struct
 import os
 
-# 将PICOVOICE_API_KEY需要放入本地环境变量中
-PICOVOICE_API_KEY = os.environ["PICOVOICE_API_KEY"]
 
-porcupine = pvporcupine.create(
-    access_key=PICOVOICE_API_KEY,
-    keyword_paths=["wakeword/Hey-cherry_en_mac_v2_2_0/Hey-cherry_en_mac_v2_2_0.ppn"],
-)
+class PicoWakeWord:
+    def __init__(self):
+        # 将PICOVOICE_API_KEY需要放入本地环境变量中
+        PICOVOICE_API_KEY = os.environ["PICOVOICE_API_KEY"]
+        self.porcupine = pvporcupine.create(
+            access_key=PICOVOICE_API_KEY,
+            keyword_paths=[
+                "wakeword/Hey-cherry_en_mac_v2_2_0/Hey-cherry_en_mac_v2_2_0.ppn"
+            ],
+        )
+        self.myaudio = pyaudio.PyAudio()
+        self.stream = self.myaudio.open(
+            input_device_index=0,
+            rate=self.porcupine.sample_rate,
+            channels=1,
+            format=pyaudio.paInt16,
+            input=True,
+            frames_per_buffer=self.porcupine.frame_length,
+        )
 
-myaudio = pyaudio.PyAudio()
-stream = myaudio.open(
-    input_device_index=0,
-    rate=porcupine.sample_rate,
-    channels=1,
-    format=pyaudio.paInt16,
-    input=True,
-    frames_per_buffer=porcupine.frame_length,
-)
+    def detect_wake_word(self):
+        audio_obj = self.stream.read(
+            self.porcupine.frame_length, exception_on_overflow=False
+        )
+        audio_obj_unpacked = struct.unpack_from(
+            "h" * self.porcupine.frame_length, audio_obj
+        )
+        keyword_idx = self.porcupine.process(audio_obj_unpacked)
+        return keyword_idx
 
-print("开始监听唤醒词...")
-while True:
-    audio_object = stream.read(porcupine.frame_length, exception_on_overflow=False)
-    audio_object_unpacked = struct.unpack_from(
-        "h" * porcupine.frame_length, audio_object
-    )
 
-    keyword_index = porcupine.process(audio_object_unpacked)
-    if keyword_index >= 0:
-        print("我听到了!")
+if __name__ == "__main__":
+    picowakeword = PicoWakeWord()
+    print("开始监听唤醒词...")
+    while True:
+        keyword_idx = picowakeword.detect_wake_word()
+        if keyword_idx >= 0:
+            print("我听到了！")
