07bd32d4ffae896fe4605903e3bf09010a49c34f
lucas-shao <lucas_shao@126.com>
2023-06-11 22:23:09 +0800

    新增唤醒的回答

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ while True:
     keyword_idx = picowakeword.detect_wake_word()
     if keyword_idx >= 0:
         print("我被唤醒了！")
+        AzureTTS().text_to_speech_and_play("主人，人家在的，请讲")
         question = AzureASR().speech_to_text()
         print("你问：{}".format(question))
         answer = question
